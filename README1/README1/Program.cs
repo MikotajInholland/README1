f2393ed92612ed97df900c21ee369ca4bcd02dea
Mikotaj Inholland <730282@student.inholland.nl>
2025-02-14 13:00:45 +0100

    jtyjdtryjfyj

--- README1/README1/Program.cs
+++ README1/README1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, 4tg34t4tg34!");
+            Console.WriteLine("kgwiyfqwjdhfewurqurougqwewq");
         }
     }
 }
