7cda4c23505aedf75f0187ce6016deaf1003cb08
Faizan731402 <731402@student.inholland.nl>
2025-02-14 12:43:29 +0100

    tg4g4tg45
    fevrgtbtrb

---
+++ README1/README1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace README1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, 4tg34t4tg34!");
+        }
+    }
+}
